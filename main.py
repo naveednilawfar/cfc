8e975345687cd8a9540f0bfad823e6ee769f0d0b
Naveed Nilawfar <naveednilawfar@gmail.com>
2021-12-29 22:36:18 +1100

    Updated code and added kv file

--- main.py
+++ main.py
@@ -6,138 +6,32 @@ from kivy.config import Config
 from kivy.lang import Builder
 from kivy.factory import Factory
 from kivy.core.window import Window
-from kivymd.uix.widget import Widget
-from kivymd.uix.label import Label
-from kivymd.uix.snackbar import SnackBar
-from kivymd.uix.button import Button
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.widget import MDAdaptiveWidget
+from kivymd.uix.snackbar import Snackbar
+import webbrowser
 
-"""CFC is a python program with kivy as the frontend GUI which converts celsius to fahrenheit or fahrenheit to celsius"""
+"""CFC is a python program with kivy as the frontend GUI which
+converts celsius to fahrenheit or fahrenheit to celsius"""
 __version__ = "1.6.5"
 __author__ = "Naveed Nilawfar"
 
-Builder.load_string("""
-<CFCLayout>
-    BoxLayout:
-        orientation:'vertical'
-        size: root.width, root.height
-        MDToolbar:
-            title: 'CFConverter'
-            md_bg_color: .2,.2,.2,1
-            specific_text_color: 1, 1, 1, 1
 
-        MDBottomNavigation:
-                    
-            MDBottomNavigationItem:
-                name: 'c2fc'
-                text: 'C2F'
-                icon: 'temperature-celsius'
-                
-                MDTextField:
-                    id: scelsius
-                    hint_text: 'Enter Celsius to convert:'
-                    input_filter: 'float'
-                    halign: 'center'
-                    font_name:'fonts/digital.ttf'
-                    size_hint_x: None
-                    mode: "rectangle"
-                    max_text_length: 15
-                    helper_text_mode: "on_focus"
-                    helper_text: "Maximum lenght is 15"
-                    width: 500
-                    pos_hint: {"center_x": 0.5, "center_y": 0.6}
-
-                MDLabel:
-                    id: afarenheit
-                    text: ""
-                    halign: 'center'
-                    font_name:'fonts/digital.ttf'
-                    size_hint_x: None
-                    width: root.width
-                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-                    
-                MDRaisedButton:
-                    text: "Caculate"
-                    font_name:'fonts/drawflygo.otf'
-                    size_hint_x: None
-                    width: 100
-                    pos_hint: {"center_x": 0.5, "center_y": 0.4}
-                    on_release: root.c2f_cal()
-
-            MDBottomNavigationItem:
-                name: 'f2cc'
-                text: 'F2C'
-                icon: 'temperature-fahrenheit'
-
-                MDTextField:
-                    id: sfarenheit
-                    hint_text: 'Enter Farenheit to convert:'
-                    input_filter: 'float'
-                    halign: 'center'
-                    font_name:'fonts/digital.ttf'
-                    size_hint_x: None
-                    mode: "rectangle"
-                    max_text_length: 16
-                    helper_text_mode: "on_focus"
-                    helper_text: "Maximum lenght is 15"
-                    width: 500
-                    pos_hint: {"center_x": 0.5, "center_y": 0.6}
-
-                MDLabel:
-                    id: acelsius
-                    text: ""
-                    halign: 'center'
-                    font_name:'fonts/digital.ttf'
-                    size_hint_x: None
-                    width: root.width
-                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-
-                MDRaisedButton:
-                    text: "Caculate"
-                    font_name:'fonts/drawflygo.otf'
-                    size_hint_x: None
-                    width: 100
-                    pos_hint: {"center_x": 0.5, "center_y": 0.4}
-                    on_release: root.f2c_cal()
-
-            MDBottomNavigationItem:
-                name: 'abt'
-                text: 'HELP'
-                icon: 'help'
-
-                MDLabel:
-                    text: "Celsius and Farenheit Converter version 1.0"
-                    font_name: 'fonts/stixtwotext.ttf'
-                    halign: 'center'
-                    pos_hint: {"center_x": 0.5, "center_y": 0.6}
-
-                MDLabel:
-                    text: "This program was created in kivy language by Naveed"
-                    font_name: 'fonts/stixtwotext.ttf'
-                    halign: 'center'
-                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
-
-                MDLabel:
-                    text: "You can find the source code of this at"
-                    font_name: 'fonts/stixtwotext.ttf'
-                    halign: 'center'
-                    pos_hint: {"center_x": 0.5, "center_y": 0.4}
-
-                MDLabel:
-                    text: "https://github.com/naveednilawfar/cfc"
-                    multiline: "true"
-                    halign: 'center'
-                    pos_hint: {"center_x": 0.5, "center_y": 0.3}
-""")
+Builder.load_file('main.kv')
 
 
-class CFCLayout(Widget):
+class CFCLayout(MDAdaptiveWidget):
     """The root Widget of CFC"""
+    def sourcecode(self, src):
+        if src == 'github':
+            webbrowser.open('https://github.com/naveednilawfar/cfc')
     def c2f_cal(self):
         """Converts celsius into farenheit"""
         self.ids.afarenheit.text = ""
         if self.ids.scelsius.text == "":
-            c2f_error = SnackBar(text="You haven't entered any value to convert into farenheit")
+            c2f_error = Snackbar(
+                text="You haven't entered any value to convert into farenheit"
+                )
+            c2f_error.open()
         else:
             answer = ""
             self.ids.afarenheit.text = ""
@@ -149,7 +43,9 @@ class CFCLayout(Widget):
         """Converts farenheit into celsius"""
         self.ids.acelsius.text = ""
         if self.ids.sfarenheit.text == "":
-            f2c_error = SnackBar(text="You haven't entered any value to convert into celsius")
+            f2c_error = Snackbar(
+                text="You haven't entered any value to convert into celsius"
+                )
             f2c_error.open()
         else:
             answer = ""
@@ -170,5 +66,5 @@ class CFCApp(MDApp):
         return CFCLayout()
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     CFCApp().run()
